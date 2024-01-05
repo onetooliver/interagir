1290e01d00e19729e6a833220cd50bb6e682cab5
Neto <neto.nelsonfsa@gmail.com>
2024-01-04 21:35:23 -0300

    update main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -10,18 +10,55 @@
         </svg>
       </div>
       <div class="container d-flex align-items-center">
-        <div class="col-6 text-main  ">
+        <div class="col-5 text-main  ">
           <h1>Colégio Interagir</h1>
           <h3>Construindo o futuro do seu filho bem pertinho de você</h3>
           <a href="https://calendly.com/neto-nelsonfsa/30min" class="btn btn-outline-success mt-5" target="_blank"  > Agendar visita</a>
         </div>
-        <div class="col-5">
+        <div class="col-4">
           <img class="img-banner" src="../../public/assets/bannerMain.png"/>
         </div>
       </div> 
     </section> 
-    <section>
-      <CardMain/>
+    <section class="container card-main">
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="card-link">Card link</a>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="card-link">Card link</a>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="card-link">Card link</a>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="card-link">Card link</a>
+        </div>
+      </div>
+    </section>
+    <section class="blog">
+      <div id="dib-posts"></div>
+    </section>
+    <section class="testemunhas">
+      <div class="elfsight-app-8460201d-2155-42c7-b1a3-74f18780890a" data-elfsight-app-lazy></div>
     </section>
   </main>
 </template>
@@ -29,35 +66,6 @@
 <style scoped>
   @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
   
-  .column-left {
-    position: relative;
-    z-index:-1;
-  }
-  
-  .container{
-    top:0;
-    margin-top:-35em;
-    z-index:5;
-    
-  }
-  .text-main{
-    
-    font-family: 'Montserrat', sans-serif;
-    margin-top:-12em;
-  }
-  .text-main h1{
-    font-size:5em;
-  }
-  .text-main h3{
-    font-size:1.5em;
-  }
-  .text-main a{
-    font-size:1.5em;
-  }
-  
-  .img-banner{
-    width:50em;
-    height:50em;
-    margin-top:-10em;
-  }
+  @import '../assets/main.css';
+  @import '../assets/main-responsivo.css';
 </style>
